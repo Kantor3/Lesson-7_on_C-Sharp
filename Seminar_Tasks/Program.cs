672e6b3e9b89a183a5ff399a4ce49124bfcd5eba
kantor <kantor3@yandex.ru>
2022-09-27 23:48:08 +0500

    changed Seminar_Tasks

--- Seminar_Tasks/Program.cs
+++ Seminar_Tasks/Program.cs
@@ -43,9 +43,7 @@ void Show2dArray(int[,] array, string text = "")
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
-        {
             Console.Write($"{array[i, j]} ");
-        }
         Console.WriteLine();
     }
     Console.WriteLine();
